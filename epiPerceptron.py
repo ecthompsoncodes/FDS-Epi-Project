ec55f596f455010283c093e1e6dd40f5d540133b
hbowman2 <157835630+hbowman2@users.noreply.github.com>
2024-02-27 16:20:14 -0500

    Add files via upload

--- epiPerceptron.py
+++ epiPerceptron.py
@@ -26,15 +26,16 @@ analysis_data = pd.read_csv("analysis_data_small.csv").values
 
 #Split the dataset into training and testing sets
 #For X, use features in column indexes 2-5; y is the last column
-X, y = analysis_data[:, 3:5], analysis_data[:, 5]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.50, random_state=42)
+X, y = analysis_data[:, 6:7], analysis_data[:, 7]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
 
 y = y.astype('bool')
 y_train = y_train.astype('bool')
 y_test = y_test.astype('bool')
 
 # Create a Perceptron classifier
-perceptron = Perceptron(random_state=42, verbose=1)
+perceptron = Perceptron(max_iter=100, eta0=0.1, random_state=42, verbose=1)
 
 # Fit the model to the data
 perceptron.fit(X_train, y_train)
