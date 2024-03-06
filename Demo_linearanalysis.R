8135a7962bd4490c4a728996744e85fcbdb61e16
Scott-VanSlyck <98994668+Scott-VanSlyck@users.noreply.github.com>
2024-03-06 16:34:28 -0500

    Add files via upload

---
+++ Demo_linearanalysis.R
@@ -0,0 +1,32 @@
+install.packages("tidyverse")
+
+library(tidyverse)
+
+
+#Read in dataset
+setwd("C:/Users/scott/OneDrive/Documents/FDS 510/Dataset")
+
+df = read_csv("data_full.csv")
+df = df[, -c(1,2,4:7,9:14,20:29)]
+
+result <- df %>%
+  group_by(county) %>%
+  summarise(
+    hotspot_prop = round(mean(Hotspot_2), 4)
+  )
+
+df1 = df %>%
+  distinct(county, .keep_all = TRUE)
+
+result = merge(result, df1, by = 'county')
+
+result = result[, -c(4, 5, 17:19)]
+
+result2 = result[, -c(1, 13, 14)]
+
+p = lm(hotspot_prop ~ ., result2)
+
+step_model = step(p)
+summary(step_model)
+
+step_model$anova
